27b024b82a43ff037a399449d72011fc871b887d
Siddharth <siddharth@vasantsoftwaressolution.com>
2023-11-28 19:07:00 +0530

    change react plate into redux plate

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://react-redux.js.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Redux
         </a>
       </header>
     </div>
